47824ff4703d55d2188712b73f62678a6c8d4988
KauanLuna <kauan.luna@sptech.school>
2025-03-19 13:27:48 -0300

    add IFNULL, NOTNULL e UNIQUE
    Foram adicionados os comandos IFNULL, NOTNULL e UNIQUE, aos campos e comandos SELECT

--- database/bdOlivGuard.sql
+++ database/bdOlivGuard.sql
@@ -17,8 +17,8 @@ CREATE TABLE descSensor(
     modelo VARCHAR(40),
     dataInstalacao DATETIME,
     localInstalacao VARCHAR(50),
-    tipoLeitura VARCHAR(30),
-    numSerie VARCHAR(20) UNIQUE
+    tipoLeitura VARCHAR(30) NOT NULL,
+    numSerie VARCHAR(20) UNIQUE NOT NULL
 );
 
 ALTER TABLE descSensor ADD CONSTRAINT chkModelo
@@ -34,18 +34,23 @@ VALUES
 
 UPDATE descSensor SET localInstalacao = 'Setor Sul' WHERE idSensor = 1;
 
-SELECT modelo AS 'Modelo do Sensor',
-date_format(dataInstalacao, '%d/%m/%Y  %H:%m') AS 'Data do Pedido', 
-localInstalacao AS 'Local de instalação', 
+UPDATE descSensor SET localInstalacao = NULL WHERE idSensor >=4;
+
+UPDATE descSensor SET dataInstalacao = NULL WHERE idSensor >=4;
+
+SELECT idSensor AS 'ID do sensor',
+modelo AS 'Modelo do Sensor',
+IFNULL(date_format(dataInstalacao, '%d/%m/%Y  %H:%m'), 'Sensor não instalado') AS 'Data do Pedido', 
+IFNULL(localInstalacao, 'Sensor não instalado') AS 'Local de instalação', 
 numSerie AS 'Número de Série', 
 tipoLeitura AS 'Tipo de Leitura' FROM descSensor;
 
 CREATE TABLE dadosSensor(
 	idDados INT PRIMARY KEY AUTO_INCREMENT,
-    dado FLOAT,
+    dado FLOAT NOT NULL,
     nivelMedicao VARCHAR(20),
-    statusSensor VARCHAR(15),
-    dtDado DATETIME
+    statusSensor VARCHAR(15) NOT NULL,
+    dtDado DATETIME NOT NULL
 );
 
 ALTER TABLE dadosSensor ADD CONSTRAINT chkStatusSensor 
@@ -100,7 +105,7 @@ CREATE TABLE cadastro(
     nome VARCHAR(50) NOT NULL,
     cpf CHAR(11) UNIQUE,
     cnpj CHAR(14) UNIQUE NOT NULL,
-    email VARCHAR(30),
+    email VARCHAR(30) NOT NULL,
     senha VARCHAR(20) NOT NULL,
     cep CHAR(8) NOT NULL,
     telefone CHAR(11)
@@ -118,10 +123,14 @@ INSERT INTO cadastro (nome, cpf, cnpj, email, senha, cep, telefone) VALUES
 
 UPDATE cadastro SET telefone = '11970156572' WHERE idCadastro = 1;
 
+UPDATE cadastro SET cpf = NULL WHERE idCadastro >=4;
+
+UPDATE cadastro SET telefone = NULL WHERE idCadastro >=4;
+
 SELECT nome AS 'Nome Usuário', 
-cpf AS 'CPF', 
+IFNULL(cpf, 'CPF não informado') AS 'CPF', 
 cnpj AS 'CNPJ', 
 email AS 'Email', 
 senha AS 'Senha', 
 cep AS 'CEP', 
-telefone AS 'Telefone' FROM cadastro;
+IFNULL(telefone, 'Telefone não informado') AS 'Telefone' FROM cadastro;
